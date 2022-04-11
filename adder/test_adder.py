8509197f1b4dd065d2a8d0af01044b83afa6f9a4
Ahmed Alsawi <you@example.com>
2022-04-11 18:30:28 +0100

    clean up files

--- adder/test_adder.py
+++ adder/test_adder.py
@@ -1,45 +1,9 @@
-# This file is public domain, it can be freely copied without restrictions.
-# SPDX-License-Identifier: CC0-1.0
-# Simple tests for an adder module
 import cocotb
 from cocotb.triggers import Timer,Edge,Join
-#from adder_model import adder_model
 from cocotb.utils import get_sim_time
 import random
 
 
-#@cocotb.test()
-#async def adder_basic_test(dut):
-#    """Test for 5 + 10"""
-#
-#    A = 5
-#    B = 10
-#
-#    dut.A.value = A
-#    dut.B.value = B
-#
-#    await Timer(2, units='ns')
-#
-#    assert dut.X.value == adder_model(A, B), f"Adder result is incorrect: {dut.X.value} != 15"
-#
-#
-#@cocotb.test()
-#async def adder_randomised_test(dut):
-#    """Test for adding 2 random numbers multiple times"""
-#
-#    for i in range(10):
-#
-#        A = random.randint(0, 15)
-#        B = random.randint(0, 15)
-#
-#        dut.A.value = A
-#        dut.B.value = B
-#
-#        await Timer(2, units='ns')
-#
-#        assert dut.X.value == adder_model(A, B), "Randomised test failed with: {A} + {B} = {X}".format(
-#            A=dut.A.value, B=dut.B.value, X=dut.X.value)
-#
 async def task(dut):
     print(f"task:{get_sim_time()}")
     await Timer(10, units='ns')
@@ -69,5 +33,5 @@ async def read_signal(dut):
     print(f"main2:{get_sim_time()}, {dut.x.value}")
 
     result = await Join(task1_coro)
-    print(f"main3:{get_sim_time()}, {dut.x.value}")
+    print(f"main3:{get_sim_time()}, {result} {dut.x.value}")
 
