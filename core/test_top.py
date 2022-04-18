799fb2a69c2cd241f6ce64d2882588f0728aa010
Ahmed Alsawi <you@example.com>
2022-04-18 21:52:47 +0100

    initial try

--- core/test_top.py
+++ core/test_top.py
@@ -1,7 +1,10 @@
+import random
+import socket
+import asyncio
+
 import cocotb
 from cocotb.triggers import Timer,Edge,Join
 from cocotb.utils import get_sim_time
-import random
 
 async def task(dut):
     print(f"task:{get_sim_time()}")
@@ -20,11 +23,41 @@ async def task2(dut):
     await  Edge(dut.x)
     print(f"task2:{get_sim_time()}")
 
+
+async def handle_client(client):
+    loop = asyncio.get_event_loop()
+    request = None
+    while request != 'quit':
+        request = (await loop.sock_recv(client, 255)).decode('utf8')
+        response = str(eval(request)) + '\n'
+        await loop.sock_sendall(client, response.encode('utf8'))
+    client.close()
+
+@cocotb.coroutine
+async def start_server(dut):
+    server = socket.socket()
+    port = 12345
+    server.bind(('', port))
+    server.listen(5)
+    server.setblocking(False)
+
+    print("starting server")
+
+    loop = asyncio.get_event_loop()
+    while True:
+        client, _ = await loop.sock_accept(server)
+        print("========================")
+        print("accept")
+        print(type(client))
+        print("========================")
+    #    loop.create_task(handle_client(client))
+
 @cocotb.test()
 async def tb_top(dut):
     task_coro  = await cocotb.start(task(dut))
     task1_coro = await cocotb.start(task1(dut))
     task2_coro = await cocotb.start(task2(dut))
+    wait_for_socket_coro = await cocotb.start(start_server(dut))
 
     print(f"main1:{get_sim_time()}, {dut.x.value}")
 
@@ -33,4 +66,7 @@ async def tb_top(dut):
 
     result = await Join(task1_coro)
     print(f"main3:{get_sim_time()}, {result} {dut.x.value}")
+
+    result = await Join(wait_for_socket_coro)
+    print(f"End:{get_sim_time()}, {result} {dut.x.value}")
 
